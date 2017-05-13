98d86cf8fc7275f83dcfb94c7975e3e33b717259
Mario Linz <gelight@gmx.de>
2017-05-14 00:39:55 +0200

    Updated header images. Created new component files ( prepared ). Updated Ember version and all addon versions.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,14 +1,14 @@
-// import Ember from 'ember';
+import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
     
-    urlForFindAll(modelName, snapshot) {
+    urlForFindAll(modelName/*, snapshot*/) {
         let baseUrl = this.buildURL();
         return baseUrl + '/rest/' + Ember.String.pluralize(modelName) + '.json';
     },
-
-    urlForFindRecord(id, modelName, snapshot) {
+    
+    urlForFindRecord(id, modelName/*, snapshot*/) {
         let baseUrl = this.buildURL();
         return baseUrl + '/rest/' + Ember.String.pluralize(modelName) + '/' + id + '.json';
     }
