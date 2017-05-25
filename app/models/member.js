b2cf0ea101ec835580b152880ad0009f249b2bdb
Mario Linz <gelight@gmx.de>
2017-05-25 13:28:42 +0200

    Improved members rest call response. Improved honor members list. Adapt index page. Prepared youth timetables rest response.

--- app/models/member.js
+++ app/models/member.js
@@ -4,31 +4,15 @@ export default DS.Model.extend({
     type: DS.attr("string"),
     firstName: DS.attr("string"),
     lastName: DS.attr("string"),
+    
     board: DS.attr("boolean"),
     boardChairman: DS.attr('boolean'),
     deputyBoardChairman: DS.attr('boolean'),
-    
-    operationsDepartment: DS.attr(),
-    honor: DS.attr('boolean'),
-    
-    representativeHonorDepartment: DS.attr('boolean'),
     treasurer: DS.attr('boolean'),
     secretary: DS.attr('boolean'),
     assessor: DS.attr('boolean'),
-    fireChief: DS.attr('boolean'),
-    operationalMembers: DS.attr('boolean'),
-    deputyFireChief: DS.attr('boolean'),
-    youthOfficer: DS.attr('boolean'),
-    childrenOfficer: DS.attr('boolean'),
-    equipmentOfficer: DS.attr('boolean'),
-    safetyOfficer: DS.attr('boolean'),
-    groupLeader: DS.attr('boolean'),
-    chiefGuard: DS.attr('boolean'),
-    militaryCommittee: DS.attr('boolean'),
-    agt: DS.attr('string'),
-    ma: DS.attr('string'),
-    ksf: DS.attr('string'),
-    operationTasks: DS.attr(),
-    troopMan: DS.attr('boolean'),
-    squadLeader: DS.attr('boolean')
+    
+    operationsDepartment: DS.attr(),
+
+    honor: DS.attr('boolean'),
 });
--- app/routes/honor.js
+++ app/routes/honor.js
@@ -6,7 +6,14 @@ export default Ember.Route.extend({
 
     model() {
         return Ember.RSVP.hash({
-            members: this.get('store').findAll('member'),
+            members: this.get('store').findAll('member').then((members) => {
+                return members.filter(function(member) {
+                    if (member.get('honor')) {
+                        return true;
+                    }
+                    return false;
+                });
+            }),
             headerSlides: [
                 {
                     img: '/img/main-1.jpg',
